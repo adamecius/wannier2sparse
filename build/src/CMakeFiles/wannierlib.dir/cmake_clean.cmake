aba27922131afef0888001e39cfe200c477efe34
Jose Hugo García <jgarcia@1605M010.icn2.net>
2019-11-12 18:25:24 +0100

    added cmake support. Tested hopping_list

---
+++ build/src/CMakeFiles/wannierlib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wannierlib.dir/wannier_parser.cpp.o"
+  "CMakeFiles/wannierlib.dir/hopping_list.cpp.o"
+  "../lib/libwannierlib.pdb"
+  "../lib/libwannierlib.so"
+  "../lib/libwannierlib.so.1.0.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wannierlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
